4ccd45f14c11fc866bfcd634bec04dfcbc72fa02
Krystle Young <krystlehyoung@gmail.com>
2017-06-07 01:31:53 -0700

    Implement index.html, about.html, Wikilinks, and prototype name.html

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var directory = __dirname + '/public';
 
 /* If we aren't on Heroku, then we need to readjust the port end directory information and we know that because port won't be set */
 if(typeof port == 'undefined' || !port){
-	directory = '.public';
+	directory = './public';
 	port = 8080;
 }
 /* Set up a static web-server that will deliver files from the filesystem  */ 
