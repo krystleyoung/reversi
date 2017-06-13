b88c493e5ccb9452cdae5964d8495132d00c82f7
Krystle Young <krystlehyoung@gmail.com>
2017-06-13 01:03:08 -0700

    Added Lobby Invite Button

--- public/js/main.js
+++ public/js/main.js
@@ -45,7 +45,7 @@ socket.on('join_room_response',function(payload){
 	var dom_elements = $('.socket_'+payload.socket_id);
 	
 	/* If we don't already have an entry for this person*/
-	if(dom_elements.length != 0){
+	if(dom_elements.length == 0){
 		var nodeA = $('<div></div>');
 		nodeA.addClass('socket_'+payload.socket_id);
 		
@@ -61,7 +61,7 @@ socket.on('join_room_response',function(payload){
 		nodeB.append('<h4>'+payload.username+'</h4>')
 
 		nodeC.addClass('col-3 text-left');
-		var buttonC = makeInviteButton();
+		var buttonC = makeInviteButton(payload.socket_id);
 		nodeC.append(buttonC);
 
 		nodeA.hide();
@@ -72,8 +72,9 @@ socket.on('join_room_response',function(payload){
 		nodeB.slideDown(1000);
 		nodeC.slideDown(1000);
 	}
+	/* If we have seen the person who just joined (something weird happened) */
 	else{
-		var buttonC = makeInviteButton();
+		var buttonC = makeInviteButton(payload.socket_id);
 		$('.socket_'+payload.socket_id+' button').replaceWith(buttonC);
 			dom_elements.slideDown(1000);
 	}
@@ -114,6 +115,32 @@ socket.on('player_disconnected',function(payload){
 	newNode.slideDown(1000);
 });
 
+/* Send an invite message to the server */
+function invite(who){
+	var payload = {};
+	payload.requested_user = who;
+	console.log('*** Client Log Message: \'invite\' payload: '+JSON.stringify(payload));
+	socket.emit('invite', payload);
+}
+
+socket.on('invite_response',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	var newNode = makeInvitedButton();
+	$('.socket_'+payload.socket_id+' button').replaceWith(newNode);
+});
+
+socket.on('invited',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	var newNode = makePlayButton();
+	$('.socket_'+payload.socket_id+' button').replaceWith(newNode);
+});
+
 
 socket.on('send_message_response',function(payload){
 	if(payload.result == 'fail'){
@@ -132,8 +159,29 @@ function send_message(){
 	socket.emit('send_message',payload);
 }
 
-function makeInviteButton(){
+function makeInviteButton(socket_id){
 	var newHTML = '<button type=\'button\' class=\'btn btn-outline-primary\'>Invite</button>';
+	var newNode = $(newHTML);
+	newNode.click(function(){
+		invite(socket_id);
+	});
+	return(newNode);
+}
+
+function makeInvitedButton(){
+	var newHTML = '<button type=\'button\' class=\'btn btn-primary\'>Invited</button>';
+	var newNode = $(newHTML);
+	return(newNode);
+}
+
+function makePlayButton(){
+	var newHTML = '<button type=\'button\' class=\'btn btn-success\'>Play</button>';
+	var newNode = $(newHTML);
+	return(newNode);
+}
+
+function makeEngageButton(){
+	var newHTML = '<button type=\'button\' class=\'btn btn-danger\'>Engaged</button>';
 	var newNode = $(newHTML);
 	return(newNode);
 }
